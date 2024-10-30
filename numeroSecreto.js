3e53ef4f8e168f185bfee995d569e6fe1496bac3
Stefany <160873683+judyjade@users.noreply.github.com>
2024-10-30 08:43:58 -0300

    js

---
+++ numeroSecreto.js
@@ -0,0 +1,23 @@
+let segredo = [2, 5, 8]
+let campoNumero = document.querySelector("input")
+campoNumero.focus()
+
+function verifica(){
+
+    let acertou = false
+
+    for(let i = 0; i < segredo.length; i++){
+        if(segredo[i] == campoNumero.value){
+            alert('Você acertou!')
+            acertou = true
+            break
+        }
+    }
+    if(acertou) {
+        alert('Você errou')
+    }
+    campoNumero = ''
+    campoNumero.focus()
+
+let botao = document.querySelector("button")
+botao.onclick = verifica
